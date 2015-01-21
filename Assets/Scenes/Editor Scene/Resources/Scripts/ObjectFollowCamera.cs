01c95c9f2dcbc28f6ba6a21c574c8ab0622c15dc
Jim <jeanmarie.baran@gmail.com>
2015-01-21 21:42:50 +0100

    Removed EditorBuildSettings from tracked files

---
+++ Assets/Scenes/Editor Scene/Resources/Scripts/ObjectFollowCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Add this script to an object to have it follow the camera.
+ */
+public class ObjectFollowCamera : MonoBehaviour {
+	
+	void Update () {
+		transform.position = new Vector3 (Camera.main.transform.position.x, Camera.main.transform.position.y, transform.position.z);
+	}
+}
