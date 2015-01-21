d967aedd8e7aece1b8c1355be2e83e66957f76b5
guixbachelier <g-bachelier@hotmail.fr>
2015-01-21 11:24:39 +0100

    Levelscene presque finie

---
+++ Assets/Scenes/Level Scene/Android/Scripts/LevelChosen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChosen : MonoBehaviour {
+
+	public int levelChosen;
+
+	// Use this for initialization
+	void Start () {
+		levelChosen = 0;
+	}
+
+	public void ValidateLevel(){
+		if(levelChosen != 0){
+			RPCWrapper.RPC("LoadWeaponLevel", RPCMode.Server);
+			Application.LoadLevel ("Android - WeaponScene");
+		}
+	}
+}
