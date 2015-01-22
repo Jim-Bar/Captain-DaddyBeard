304cdc4f09b9432fa1c21f63e245b45a77c7b7c7
erwan94 <erwan-munier@live.fr>
2015-01-22 02:16:12 +0100

    modif move scene

---
+++ Assets/Scenes/Move Scene/Windows/Scripts/JumpPlatform.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpPlatform : MonoBehaviour 
+{
+
+	[SerializeField] private GameObject sphere;
+
+	public void ControllerEnter2D(CircleCollider2D controller)
+	{
+
+		sphere.rigidbody2D.AddForce(new Vector2(0,100F), ForceMode2D.Force);
+	}
+}
