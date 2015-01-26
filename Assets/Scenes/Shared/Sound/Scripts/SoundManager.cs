9eb49b38d6e8ee9826f450022656b33a1fecf53c
guixbachelier <g-bachelier@hotmail.fr>
2015-01-26 13:47:08 +0100

    déplacement 1.1, ajout son

---
+++ Assets/Scenes/Shared/Sound/Scripts/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+
+	[SerializeField] private GameObject jump;
+	[SerializeField] private GameObject validate;
+	[SerializeField] private GameObject theme;
+	[SerializeField] private GameObject shoot;
+	[SerializeField] private GameObject explode;
+	
+	// Use this for initialization
+	void Start () {
+		jump.audio.Stop ();
+		validate.audio.Stop ();
+		theme.audio.Stop ();
+		shoot.audio.Stop ();
+		explode.audio.Stop ();
+		theme.audio.Play ();
+	}
+
+	public void SoundJump(){
+		jump.audio.Play ();
+	}
+
+	public void SoundValidate(){
+		validate.audio.Play ();
+	}
+
+	public void SoundShoot(){
+		shoot.audio.Play ();
+	}
+
+	public void SoundExplode(){
+		explode.audio.Play ();
+	}
+}
