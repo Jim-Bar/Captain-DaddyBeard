70fed5a1ead50f39922d0f279d8b58d308e6f2cc
guixbachelier <g-bachelier@hotmail.fr>
2015-02-02 19:39:34 +0100

    Skip Button on Android Weapon

--- Assets/Scenes/Weapon Scene/Android/Scripts/DragAndSend.cs
+++ Assets/Scenes/Weapon Scene/Android/Scripts/DragAndSend.cs
@@ -5,9 +5,10 @@ using UnityEngine.EventSystems;
 
 public class DragAndSend : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
+	[SerializeField] private int weaponNumber;
 	public static GameObject itemBeingDragged;
 	Vector3 startPosition;
-	Vector2 touchPosition;
+	//Vector2 touchPosition;
 	Transform startParent;
 
 	#region IBeginDragHandler implementation
@@ -16,15 +17,17 @@ public class DragAndSend : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 	{
 		itemBeingDragged = gameObject;
 		startPosition = transform.position;
-		startParent = transform.parent;
-		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+		//startParent = transform.parent;
+		//GetComponent<CanvasGroup> ().blocksRaycasts = false;
 	}
 
 	#region IDragHandler implementation
 	
 	public void OnDrag (PointerEventData eventData)
 	{
-		transform.position = touchPosition;
+		//transform.position = touchPosition;
+		transform.position = Input.mousePosition;
+		Debug.Log (transform.position.y);
 	}
 	
 	#endregion
@@ -37,20 +40,24 @@ public class DragAndSend : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 	{
 		itemBeingDragged = null;
 		GetComponent<CanvasGroup> ().blocksRaycasts = true;
-		if(transform.parent == startParent){
-			transform.position = startPosition;
-		}
+			if(transform.position.y > 320){
+				Debug.Log ("Send the message");
+				//call RPC function here
+				//RPCWrapper.RPC ("AddWeapon", RPCMode.Server, Player.id.Get (), weaponNumber);
+			}
+
+		transform.position = startPosition;
 	}
 	
 	#endregion
 
 	// Update is called once per frame
-	void Update () {
+	/*void Update () {
 		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
 			touchPosition = Input.GetTouch(0).deltaPosition;
 		}
 
-	}
+	}*/
 
 
 
