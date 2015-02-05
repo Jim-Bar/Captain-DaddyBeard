53e681529295f0e5525c1481da55d95302f2e082
guixbachelier <g-bachelier@hotmail.fr>
2015-02-05 14:32:20 +0100

    visee weponscene

---
+++ Assets/Scenes/Weapon Scene/Windows/Scripts/ToTheGame.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToTheGame : MonoBehaviour {
+	
+	// Reference towards the target.
+	[SerializeField] private GameObject target;
+	
+	// If the target is on the object.
+	private bool targetted = false;
+	
+	void Start () {
+		//this line is very important, that is the way we use to call any sound from the sound manager
+	}
+	
+	// Launch the game if the play "button" is targetted when the user press the button on his tablet.
+	public void TryLaunchingGame () {
+		if (targetted){
+			RPCWrapper.RPC ("ToTheGameAndroidFunc", RPCMode.Others);
+			PhaseLoader.Load ();
+		}
+	}
+	
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject == target)
+			targetted = true;
+	}
+	
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.gameObject == target)
+			targetted = false;
+	}
+}
