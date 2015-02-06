53666dd0994193c8b29c66d25a22dbbc82bea238
Jeremy948 <jeremy.lej@gmail.com>
2015-02-06 14:52:07 +0100

    daddy sprite

--- Assets/Scenes/Shoot Scene/Android/Scripts/BonusSpawner.cs
+++ Assets/Scenes/Shoot Scene/Android/Scripts/BonusSpawner.cs
@@ -59,7 +59,7 @@ public class BonusSpawner : MonoBehaviour {
 					break;
 				}
 
-		go = Network.Instantiate (go, new Vector3 (Camera.main.transform.position.x + 20, Random.Range(4,8), 3.5f), Quaternion.identity, 0) as GameObject;
+		go = Network.Instantiate (go, new Vector3 (Camera.main.transform.position.x + 20, Random.Range(4,8), 2.5f), Quaternion.identity, 0) as GameObject;
 		if (Random.Range (0, 3) < 1)
 			Destroy (go.transform.GetChild (0).gameObject);
 		go.transform.parent = gameObject.transform;	
