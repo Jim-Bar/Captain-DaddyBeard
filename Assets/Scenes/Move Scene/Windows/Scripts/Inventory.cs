5304126114bbbcdbbb9e038333ca9e5338c3096e
erwan94 <erwan-munier@live.fr>
2015-02-07 15:02:49 +0100

    ajout bonus score et life

---
+++ Assets/Scenes/Move Scene/Windows/Scripts/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+	private int nbBonusScore = 0;
+	public UnityEngine.UI.Text displayText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (nbBonusScore >= 10) {
+			displayText.text = "X " + nbBonusScore.ToString();
+
+		} 
+		else {
+			displayText.text = "X 0" + nbBonusScore.ToString();
+		}
+	
+	}
+
+	//incremente le nombre de bonus score
+	public void IncBonusScore() {
+		nbBonusScore++;
+	}
+}
