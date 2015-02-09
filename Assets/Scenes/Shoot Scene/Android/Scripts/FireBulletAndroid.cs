8fe92cb15fbfeed3b8263fc6f3d21c65e6180e9b
Jim <jeanmarie.baran@gmail.com>
2015-02-09 20:25:15 +0100

    Fixed sound problem when attempting to fire with 0 energy

--- Assets/Scenes/Shoot Scene/Android/Scripts/FireBulletAndroid.cs
+++ Assets/Scenes/Shoot Scene/Android/Scripts/FireBulletAndroid.cs
@@ -9,12 +9,19 @@ public class FireBulletAndroid : MonoBehaviour {
 	
 	[Tooltip("Speed of the bullet")]
 	[SerializeField] private float bulletSpeed = 1;
+
+	private SoundManager soundManager = null;
 	
 	private void Start () {
+		soundManager = GameObject.Find ("SoundManager").GetComponent<SoundManager> ();
+		if (soundManager == null)
+			Debug.LogError (GetType ().Name + " : Sound manager not found.");
+
 		RPCWrapper.RegisterMethod (InstanciateShoot);
 	}
 	
 	private void InstanciateShoot (Vector3 targetPosition) {
+		soundManager.SoundShoot();
 		GameObject bullet = Instantiate (bulletPrefab, transform.position + 0.1f * Vector3.forward, Quaternion.identity) as GameObject;
 		bullet.rigidbody2D.velocity = bulletSpeed * (targetPosition - transform.position).normalized;
 		bullet.rigidbody2D.angularVelocity = Random.Range (-360, 360);
--- Assets/Scenes/Shoot Scene/Android/Scripts/Shoot.cs
+++ Assets/Scenes/Shoot Scene/Android/Scripts/Shoot.cs
@@ -5,6 +5,5 @@ public class Shoot : MonoBehaviour {
 	
 	public void ShootButtonPressed () {
 		RPCWrapper.RPC ("ShootPressed", RPCMode.Server);
-		GameObject.Find ("SoundManager").GetComponent<SoundManager> ().SoundShoot();
 	}
 }
