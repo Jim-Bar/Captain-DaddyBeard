2536aa6a9a936e9f4015e8d96cd5d59a8dcbdcae
guixbachelier <g-bachelier@hotmail.fr>
2015-02-09 21:10:40 +0100

    update weapon display

--- Assets/Scenes/Shoot Scene/Android/Scripts/FireBulletAndroid.cs
+++ Assets/Scenes/Shoot Scene/Android/Scripts/FireBulletAndroid.cs
@@ -1,18 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 
 public class FireBulletAndroid : MonoBehaviour {
 		
-	[Tooltip("Prefab of a bullet")]
-	[SerializeField] private GameObject bulletPrefab = null;
+	//[Tooltip("Prefab of a bullet")]
+	//[SerializeField] private GameObject bulletPrefab = null;
+	private GameObject bulletPrefab = null;
 	
 	[Tooltip("Speed of the bullet")]
 	[SerializeField] private float bulletSpeed = 1;
 
+	private Image image;
+	[SerializeField] private Sprite weapon1 = null;
+	[SerializeField] private Sprite weapon2 = null;
+	[SerializeField] private Sprite weapon3 = null;
+	[SerializeField] private GameObject prefab1 = null;
+	[SerializeField] private GameObject prefab2 = null;
+	[SerializeField] private GameObject prefab3 = null;
+
 	private SoundManager soundManager = null;
 	
 	private void Start () {
+		switch(Player.weapon1.Get()){
+		case 2:
+			image.sprite = weapon2;
+			bulletPrefab = prefab2;
+			break;
+		case 3:
+			image.sprite = weapon3;
+			bulletPrefab = prefab3;
+			break;
+		default:
+			image.sprite = weapon1;
+			bulletPrefab = prefab1;
+			break;
+		}
 		soundManager = GameObject.Find ("SoundManager").GetComponent<SoundManager> ();
 		if (soundManager == null)
 			Debug.LogError (GetType ().Name + " : Sound manager not found.");
@@ -24,6 +48,8 @@ public class FireBulletAndroid : MonoBehaviour {
 		soundManager.SoundShoot();
 		GameObject bullet = Instantiate (bulletPrefab, transform.position + 0.1f * Vector3.forward, Quaternion.identity) as GameObject;
 		bullet.rigidbody2D.velocity = bulletSpeed * (targetPosition - transform.position).normalized;
-		bullet.rigidbody2D.angularVelocity = Random.Range (-360, 360);
+		if(Player.weapon1.Get() == 1){
+			bullet.rigidbody2D.angularVelocity = Random.Range (-360, 360);
+		}
 	}
 }
--- Assets/Scenes/Shoot Scene/Windows/Scripts/FireBullet.cs
+++ Assets/Scenes/Shoot Scene/Windows/Scripts/FireBullet.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 /*
@@ -11,13 +12,36 @@ public class FireBullet : MonoBehaviour {
 	[Tooltip("Reference towards the target")]
 	[SerializeField] private GameObject target = null;
 
-	[Tooltip("Prefab of a bullet")]
-	[SerializeField] private GameObject bulletPrefab = null;
+	//[Tooltip("Prefab of a bullet")]
+	//[SerializeField] private GameObject bulletPrefab = null;
+	private GameObject bulletPrefab = null;
 
 	[Tooltip("Speed of the bullet")]
 	[SerializeField] private float bulletSpeed = 1;
 
+	private Image image;
+	[SerializeField] private Sprite weapon1 = null;
+	[SerializeField] private Sprite weapon2 = null;
+	[SerializeField] private Sprite weapon3 = null;
+	[SerializeField] private GameObject prefab1 = null;
+	[SerializeField] private GameObject prefab2 = null;
+	[SerializeField] private GameObject prefab3 = null;
+
 	private void Start () {
+		switch(Player.weapon1.Get()){
+			case 2:
+				image.sprite = weapon2;
+				bulletPrefab = prefab2;
+				break;
+			case 3:
+				image.sprite = weapon3;
+				bulletPrefab = prefab3;
+				break;
+			default:
+				image.sprite = weapon1;
+				bulletPrefab = prefab1;
+				break;
+		}
 		if (target == null)
 			Debug.LogError (GetType ().Name + " : The field 'target' is empty");
 
@@ -32,7 +56,9 @@ public class FireBullet : MonoBehaviour {
 			Player.energy1.Burn (100);
 			GameObject bullet = Instantiate (bulletPrefab, transform.position + 0.1f * Vector3.forward, Quaternion.identity) as GameObject;
 			bullet.rigidbody2D.velocity = bulletSpeed * (target.transform.position - transform.position).normalized;
-			bullet.rigidbody2D.angularVelocity = Random.Range (-360, 360);
+			if(Player.weapon1.Get() == 1){
+				bullet.rigidbody2D.angularVelocity = Random.Range (-360, 360);
+			}
 			RPCWrapper.RPC ("InstanciateShoot", RPCMode.Others, target.transform.position);
 		}
 
