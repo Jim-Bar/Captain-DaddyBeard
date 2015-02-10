8abea6f1214055863789f36d3ffd112b9065b0c1
guixbachelier <g-bachelier@hotmail.fr>
2015-02-10 03:31:53 +0100

    update level scene

--- Assets/Scenes/Level Scene/Android/Scripts/ChangeColorLevel.cs
+++ Assets/Scenes/Level Scene/Android/Scripts/ChangeColorLevel.cs
@@ -4,36 +4,37 @@ using System.Collections;
 
 public class ChangeColorLevel : MonoBehaviour {
 
-	[SerializeField] private Sprite validated = null;
-	[SerializeField] private Sprite nonValidated = null;
+	//[SerializeField] private Sprite validated = null;
+	//[SerializeField] private Sprite nonValidated = null;
 	[SerializeField] private int levelNumber;
 	private Image image;
 
 	// Use this for initialization
 	void Start () {
 		image = gameObject.GetComponent<Image> ();
-		image.sprite = nonValidated;
+		image.color = Color.red;
+		//image.sprite = nonValidated;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if(GameObject.Find ("Button").GetComponent<LevelChosen>().levelChosen == levelNumber){
-			image.sprite = validated;
+			image.color = Color.green;
 		}
 		else{
-			image.sprite = nonValidated;
+			image.color = Color.red;
 		}
 		
 	}
 
 	public void OnLevelPressed(){
-		if (image.sprite == nonValidated) {
-			image.sprite = validated;
+		if (image.color == Color.red) {
+			image.color = Color.green;;
 			GameObject.Find ("Button").GetComponent<LevelChosen>().levelChosen = levelNumber;
 			RPCWrapper.RPC("UpdateColorWin", RPCMode.Server, levelNumber);
 		}
 		else{
-			image.sprite = nonValidated;
+			image.color = Color.red;
 			GameObject.Find ("Button").GetComponent<LevelChosen>().levelChosen = 0;
 			RPCWrapper.RPC("UpdateColorWin", RPCMode.Server, 0);
 		}
--- Assets/Scenes/Level Scene/Windows/Scripts/UpdateLevelWindows.cs
+++ Assets/Scenes/Level Scene/Windows/Scripts/UpdateLevelWindows.cs
@@ -7,48 +7,58 @@ public class UpdateLevelWindows : MonoBehaviour {
 
 	private Image image;
 	public int levelChoice;
-	[SerializeField] private Sprite nochoice = null;
-	[SerializeField] private Sprite level1 = null;
-	[SerializeField] private Sprite level2 = null;
-	[SerializeField] private Sprite level3 = null;
-	[SerializeField] private Sprite level4 = null;
-	[SerializeField] private Sprite level5 = null;
-	[SerializeField] private Sprite level6 = null;
+	[SerializeField] private Text number = null;
+	//[SerializeField] private Sprite nochoice = null;
+	//[SerializeField] private Sprite level1 = null;
+	//[SerializeField] private Sprite level2 = null;
+	//[SerializeField] private Sprite level3 = null;
+	//[SerializeField] private Sprite level4 = null;
+	//[SerializeField] private Sprite level5 = null;
+	//[SerializeField] private Sprite level6 = null;
 	// Use this for initialization
 	void Start () {
 		levelChoice = 0;
+		number = GameObject.Find ("Number").GetComponent<Text> ();
 		image = gameObject.GetComponent<Image>();
+		image.color = Color.red;
 		RPCWrapper.RegisterMethod (UpdateColorWin);
 	}
 
 	public void UpdateColorWin (int level){
 		switch (level) {
 			case 1:
-				image.sprite = level1;
+				image.color = Color.green;
+				number.text = level.ToString();
 				levelChoice = 1;
 				break;
 			case 2:
-				image.sprite = level2;
+				image.color = Color.green;
+				number.text = level.ToString();
 				levelChoice = 2;
 				break;
 			case 3:
-				image.sprite = level3;
+				image.color = Color.green;
+				number.text = level.ToString();
 				levelChoice = 3;
 				break;
 			case 4:
-				image.sprite = level4;
+				image.color = Color.green;
+				number.text = level.ToString();
 				levelChoice = 4;
 				break;
 			case 5:
-				image.sprite = level5;
+				image.color = Color.green;
+				number.text = level.ToString();
 				levelChoice = 5;
 				break;
 			case 6:
-				image.sprite = level6;
+				image.color = Color.green;
+				number.text = level.ToString();
 				levelChoice = 6;
 				break;
 			default:
-				image.sprite = nochoice;
+				image.color = Color.red;
+				number.text = null;
 				levelChoice = 0;
 				break;
 		}
