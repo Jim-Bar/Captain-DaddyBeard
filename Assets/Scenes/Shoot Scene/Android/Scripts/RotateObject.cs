66905abf09f90327c92804a25b59790e03f974cc
Jim <jeanmarie.baran@gmail.com>
2015-02-10 13:29:46 +0100

    Changed filter in scan mode

---
+++ Assets/Scenes/Shoot Scene/Android/Scripts/RotateObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Rotate an object permanently.
+public class RotateObject : MonoBehaviour {
+
+	[SerializeField] private float speed = 100;
+
+	void Update () {
+		transform.Rotate (speed * Time.deltaTime * Vector3.forward);
+	}
+}
