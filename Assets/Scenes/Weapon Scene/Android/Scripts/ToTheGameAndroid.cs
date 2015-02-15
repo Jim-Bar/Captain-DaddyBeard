73cfb8a301aa6c5f550bdccaa3a7d476890f68da
Jim <jeanmarie.baran@gmail.com>
2015-02-15 02:07:27 +0100

    UI responsive

--- Assets/Scenes/Weapon Scene/Android/Scripts/ToTheGameAndroid.cs
+++ Assets/Scenes/Weapon Scene/Android/Scripts/ToTheGameAndroid.cs
@@ -4,11 +4,8 @@ using UnityEngine.UI;
 
 public class ToTheGameAndroid : MonoBehaviour {
 
-	[SerializeField] public Text carac;
-
 	void Start () {
 		RPCWrapper.RegisterMethod (ToTheGameAndroidFunc);
-		carac.enabled = false;
 	}
 
 	public void OnButtonPressedAndroid(){
@@ -18,27 +15,5 @@ public class ToTheGameAndroid : MonoBehaviour {
 
 	public void ToTheGameAndroidFunc(){
 		PhaseLoader.Load ();
-	}
-
-	public void ShowText(int weapon){
-		switch(weapon){
-			case 2 :
-				carac.text = ("Weapon2 : Damage 2, Energy -2");
-				break;
-			case 3 :
-				carac.text = ("Weapon3 : Damage 3, Energy -3");
-				break;
-			case 4 :
-				carac.text = ("Weapon4 : Damage 4, Energy -4");
-				break;
-			default :
-				carac.text = ("Gun : Damage 1, Energy -1");
-				break;
-		}
-		carac.enabled = true;
-	}
-
-	public void HideText(){
-		carac.enabled = false;
 	}
 }
---
+++ Assets/Scenes/Weapon Scene/Android/Scripts/WeaponSelection.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class WeaponSelection : MonoBehaviour {
+
+	[SerializeField] private int weaponNumber;
+
+	public void SelectWeapon () {
+		Player.weapon1.Set (weaponNumber);
+		RPCWrapper.RPC ("AddWeapon", RPCMode.Server, Player.id.Get (), weaponNumber);
+	}
+}
